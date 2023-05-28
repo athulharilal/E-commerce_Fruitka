355b8f18d0d269cbbce2f9ffd5ad03b7fbba5833
athulharilal <75790029+athulharilal@users.noreply.github.com>
2023-05-28 17:01:32 +0530

    dependency issue

--- app.js
+++ app.js
@@ -66,7 +66,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ secret: secret, cookie: { maxAge: 6000000 } }));
+app.use(session({
+  secret: secret,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 6000000 }
+}));
+
+
 
 db.connect((err) => {
   if (err) {
